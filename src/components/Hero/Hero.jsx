ca7739c8ab004bac4c69164cdb33aaa0ff793b31
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-12-13 09:47:06 +0530

    feat: Added carousel in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,45 @@
+import React, { useState } from "react";
 import "./hero.css";
-import { SlCalender } from "react-icons/sl";
-import { FaUser } from "react-icons/fa";
-import { HERO_IMG, HERO_IMG_SLIDER } from "../../constants";
-import HERO_BRAND from "../../assets/brand.svg"
+import { HERO_IMG, HERO_IMG_SLIDER, CAROUSEL_IMAGES } from "../../constants";
+import HERO_BRAND from "../../assets/brand.svg";
+import { TbPoint } from "react-icons/tb";
+import { TbPointFilled } from "react-icons/tb";
 
 export const Hero = () => {
+  const [currentImg, setCurrentImage] = useState(0); // State to track current carousel image
+
+  // Function to update carousel image
+  const changeCarouselImage = (i) => {
+    setCurrentImage(i);
+  };
   return (
     <div>
       <div className="hero_container">
-        <img className="hero_img" alt="hero_images" src={HERO_IMG} />
+        <img
+          className="hero_img"
+          alt="hero_images"
+          src={CAROUSEL_IMAGES[currentImg]}
+        />
         <div className="brand_heading">
           <h4 className="hero_title">Sea La Vie</h4>
           <p>Alibaug, Maharashtra</p>
           <div className="hero_action">
-            <img src={HERO_BRAND} alt="hero_action" width="100%"/>
+            <img src={HERO_BRAND} alt="hero_action" width="100%" />
+          </div>
+        </div>
+        <div className="carousel_tracker">
+          <div className="flex">
+            {CAROUSEL_IMAGES?.map((img, i) => {
+              return (
+                <span Key={i} onClick={() => changeCarouselImage(i)}>
+                  {currentImg === i ? (
+                    <TbPointFilled className="carousel_icon" />
+                  ) : (
+                    <TbPoint className="carousel_icon" />
+                  )}
+                </span>
+              );
+            })}
           </div>
         </div>
         <div className="brand_img_slider">
@@ -22,7 +48,6 @@ export const Hero = () => {
             <p>+12</p>
           </div>
         </div>
-
       </div>
     </div>
   );
